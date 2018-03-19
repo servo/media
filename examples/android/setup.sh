48a06ed630d6cf5fd3914808bbc2914f7ede2ca8
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-03-19 13:26:20 +0100

    Add setup script to download gstreamer generated assets

---
+++ examples/android/setup.sh
@@ -0,0 +1,6 @@
+wget https://github.com/ferjm/libgstreamer_android_gen/blob/gh-pages/out/src.zip?raw=true -O src.zip
+unzip src.zip -d src_
+mv src_/src/org src/app/src/main/java/
+mv src_/src/main/* src/app/src/main/
+rm -rf src.zip
+rm -rf src_
