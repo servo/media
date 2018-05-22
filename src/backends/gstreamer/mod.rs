61ca7820b22e44085c2bf1681080c14b7d610c54
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-05-22 17:16:59 +0200

    Remove unused extern crates

--- src/backends/gstreamer/mod.rs
+++ src/backends/gstreamer/mod.rs
@@ -1,8 +1,5 @@
-extern crate glib;
-extern crate gst_plugin;
 extern crate gstreamer_app as gst_app;
 extern crate gstreamer_audio as gst_audio;
-extern crate gstreamer_base as gst_base;
 
 pub mod audio_sink;
 
