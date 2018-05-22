852e78539568442a118c61ec28e6c72b9562f285
Manish Goregaokar <manishsmail@gmail.com>
2018-05-22 14:13:02 -0700

    Use correct block size

---
+++ src/audio/block.rs
@@ -0,0 +1,5 @@
+// defined by spec
+// https://webaudio.github.io/web-audio-api/#render-quantum
+pub const FRAMES_PER_BLOCK: u8 = 128;
+
+
--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,6 +1,8 @@
 extern crate byte_slice_cast;
 extern crate num_traits;
 
+
+pub mod block;
 pub mod graph;
 pub mod graph_thread;
 pub mod node;
