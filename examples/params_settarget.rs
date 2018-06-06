b932b0dfd6b190848ae5e9af2f46425d4712882a
Manish Goregaokar <manishsmail@gmail.com>
2018-06-06 13:06:37 -0700

    Use NodeID abstraction, return node ids

--- examples/params_settarget.rs
+++ examples/params_settarget.rs
@@ -9,22 +9,22 @@ use std::{thread, time};
 fn main() {
     if let Ok(servo_media) = ServoMedia::get() {
         let mut graph = servo_media.create_audio_graph();
-        graph.create_node(AudioNodeType::OscillatorNode(Default::default()));
+        let osc = graph.create_node(AudioNodeType::OscillatorNode(Default::default()));
         graph.resume();
         graph.message_node(
-            0,
+            osc,
             AudioNodeMessage::OscillatorNode(OscillatorNodeMessage::Start(0.)),
         );
         // 0.1s: Set frequency to 110Hz
         graph.message_node(
-            0,
+            osc,
             AudioNodeMessage::OscillatorNode(OscillatorNodeMessage::SetFrequency(
                 UserAutomationEvent::SetValueAtTime(110., 0.1),
             )),
         );
         // 0.3s: Start increasing frequency to 440Hz exponentially with a time constant of 1
         graph.message_node(
-            0,
+            osc,
             AudioNodeMessage::OscillatorNode(OscillatorNodeMessage::SetFrequency(
                 UserAutomationEvent::SetTargetAtTime(440., 0.3, 1.),
             )),
@@ -33,14 +33,14 @@ fn main() {
         // this event effectively doesn't happen, but instead sets a starting point
         // for the next ramp event
         graph.message_node(
-            0,
+            osc,
             AudioNodeMessage::OscillatorNode(OscillatorNodeMessage::SetFrequency(
                 UserAutomationEvent::SetTargetAtTime(1760., 1.5, 0.1),
             )),
         );
         // 1.5s - 3s Linearly ramp down from the previous event (1.5s) to 110Hz
         graph.message_node(
-            0,
+            osc,
             AudioNodeMessage::OscillatorNode(OscillatorNodeMessage::SetFrequency(
                 UserAutomationEvent::RampToValueAtTime(RampKind::Linear, 110., 3.0),
             )),
