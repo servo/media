2b8bda037cd35dd1793dce71b01ab32707fa3d66
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-06-18 17:03:28 +0200

    Remove unused AudioDecoderMsg

--- servo-media/src/audio/decoder.rs
+++ servo-media/src/audio/decoder.rs
@@ -75,13 +75,6 @@ impl AudioDecoderCallbacksBuilder {
     }
 }
 
-pub enum AudioDecoderMsg {
-    Eos,
-    Error,
-    // XXX Avoid copying :\
-    Progress(Vec<f32>),
-}
-
 pub trait AudioDecoder {
     fn decode(&self, data: Vec<u8>, callbacks: AudioDecoderCallbacks);
 }
