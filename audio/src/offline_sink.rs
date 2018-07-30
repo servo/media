bdd033e40659b161938223dc452487da06fc76cf
Manish Goregaokar <manishsmail@gmail.com>
2018-07-30 15:20:03 -0700

    Correctly render the last block of an offline audio context
    It may not be a full block length.

--- audio/src/offline_sink.rs
+++ audio/src/offline_sink.rs
@@ -51,37 +51,40 @@ impl AudioSink for OfflineAudioSink {
 
     fn has_enough_data(&self) -> bool {
         self.has_enough_data.get()
-            || (self.rendered_blocks.get() >= (self.length / FRAMES_PER_BLOCK_USIZE))
+            || (self.rendered_blocks.get() * FRAMES_PER_BLOCK_USIZE >= self.length)
     }
 
     fn push_data(&self, mut chunk: Chunk) -> Result<(), ()> {
-        {
-            let offset = self.rendered_blocks.get() * FRAMES_PER_BLOCK_USIZE;
-            let mut buffer = self.buffer.borrow_mut();
-            if buffer.is_none() {
-                *buffer = Some(vec![0.; self.channel_count * self.length]);
-            }
-            if chunk.len() == 0 {
-                chunk.blocks.push(Default::default());
-            }
-            if chunk.blocks[0].is_empty() {
-                chunk.blocks[0].explicit_silence();
-            }
-            if let Some(ref mut buffer) = *buffer {
-                for channel_number in 0..self.channel_count {
-                    let channel_offset = offset + (channel_number * self.length);
-                    let mut channel_data =
-                        &mut buffer[channel_offset..channel_offset + FRAMES_PER_BLOCK_USIZE];
-                    channel_data.copy_from_slice(chunk.blocks[0].data_chan(channel_number as u8));
-                }
-            };
-            self.rendered_blocks.update(|blocks| blocks + 1);
+        let offset = self.rendered_blocks.get() * FRAMES_PER_BLOCK_USIZE;
+        let (last, copy_len) = if self.length - offset <= FRAMES_PER_BLOCK_USIZE {
+            (true, self.length - offset)
+        } else {
+            (false, FRAMES_PER_BLOCK_USIZE)
+        };
+        let mut buffer = self.buffer.borrow_mut();
+        if buffer.is_none() {
+            *buffer = Some(vec![0.; self.channel_count * self.length]);
         }
+        if chunk.len() == 0 {
+            chunk.blocks.push(Default::default());
+        }
+        if chunk.blocks[0].is_empty() {
+            chunk.blocks[0].explicit_silence();
+        }
+        if let Some(ref mut buffer) = *buffer {
+            for channel_number in 0..self.channel_count {
+                let channel_offset = offset + (channel_number * self.length);
+                let mut channel_data =
+                    &mut buffer[channel_offset..channel_offset + copy_len];
+                channel_data.copy_from_slice(&chunk.blocks[0].data_chan(channel_number as u8)[0..copy_len]);
+            }
+        };
+        self.rendered_blocks.update(|blocks| blocks + 1);
 
-        if self.rendered_blocks.get() >= (self.length / FRAMES_PER_BLOCK_USIZE) {
+        if last {
             if let Some(callback) = self.eos_callback.borrow_mut().take() {
                 let processed_audio =
-                    ProcessedAudio(self.buffer.borrow_mut().take().unwrap().into_boxed_slice());
+                    ProcessedAudio(buffer.take().unwrap().into_boxed_slice());
                 callback(Box::new(processed_audio));
             }
         }
