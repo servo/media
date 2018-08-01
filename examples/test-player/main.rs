cd7bf4a06251c91b88fe87faeafd14dad0d21cd8
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-08-01 19:05:47 +0200

    Test player

---
+++ examples/test-player/main.rs
@@ -0,0 +1,196 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#![feature(rustc_private)]
+
+extern crate gleam;
+extern crate glutin;
+extern crate servo_media;
+extern crate time;
+extern crate webrender;
+extern crate winit;
+
+use gleam::gl;
+use servo_media::player::frame::{Frame, FrameRenderer};
+use servo_media::player::{Player, PlayerEvent};
+use servo_media::ServoMedia;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::path::Path;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread::Builder;
+use webrender::api::*;
+
+#[path = "ui.rs"]
+mod ui;
+
+struct PlayerWrapper {
+    player: Arc<Mutex<Box<Player>>>,
+    shutdown: Arc<AtomicBool>,
+}
+
+impl PlayerWrapper {
+    pub fn new(path: &Path) -> Self {
+        let servo_media = ServoMedia::get().unwrap();
+        let player = Arc::new(Mutex::new(servo_media.create_player().unwrap()));
+        let file = File::open(&path).unwrap();
+        let metadata = file.metadata().unwrap();
+        player.lock().unwrap().set_input_size(metadata.len());
+        let (sender, receiver) = mpsc::channel();
+        player.lock().unwrap().register_event_handler(sender);
+        player
+            .lock()
+            .unwrap()
+            .setup()
+            .expect("couldn't setup player");
+        let player_ = player.clone();
+        let player__ = player.clone();
+        let shutdown = Arc::new(AtomicBool::new(false));
+        let shutdown_ = shutdown.clone();
+        let shutdown__ = shutdown.clone();
+        Builder::new()
+            .name("File reader".to_owned())
+            .spawn(move || {
+                let player = &player_;
+                let shutdown = &shutdown_;
+                let mut buf_reader = BufReader::new(file);
+                let mut buffer = [0; 8192];
+                while !shutdown.load(Ordering::Relaxed) {
+                    match buf_reader.read(&mut buffer[..]) {
+                        Ok(0) => {
+                            println!("finished pushing data");
+                            break;
+                        }
+                        Ok(size) => {
+                            if let Err(_) = player
+                                .lock()
+                                .unwrap()
+                                .push_data(Vec::from(&buffer[0..size]))
+                            {
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Error: {}", e);
+                            break;
+                        }
+                    }
+                }
+            })
+            .unwrap();
+
+        Builder::new()
+            .name("Player event loop".to_owned())
+            .spawn(move || {
+                let player = &player__;
+                let shutdown = &shutdown__;
+                while let Ok(event) = receiver.recv() {
+                    match event {
+                        PlayerEvent::EndOfStream => {
+                            println!("EOF");
+                            break;
+                        }
+                        PlayerEvent::Error => {
+                            println!("Error");
+                            break;
+                        }
+                        PlayerEvent::MetadataUpdated(ref m) => {
+                            println!("Metadata updated! {:?}", m);
+                        }
+                        PlayerEvent::StateChanged(ref s) => {
+                            println!("Player state changed to {:?}", s);
+                        }
+                        PlayerEvent::FrameUpdated => eprint!("."),
+                    }
+                }
+                player.lock().unwrap().stop();
+                shutdown.store(true, Ordering::Relaxed);
+            })
+            .unwrap();
+
+        player.lock().unwrap().play();
+
+        PlayerWrapper { player, shutdown }
+    }
+
+    fn shutdown(&self) {
+        self.player.lock().unwrap().stop();
+        self.shutdown.store(true, Ordering::Relaxed);
+    }
+
+    fn register_frame_renderer(&self, renderer: Arc<FrameRenderer>) {
+        self.player
+            .lock()
+            .unwrap()
+            .register_frame_renderer(renderer);
+    }
+}
+
+struct App {
+    frame_queue: Mutex<Vec<Frame>>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            frame_queue: Mutex::new(Vec::new()),
+        }
+    }
+}
+
+impl ui::Example for App {
+    fn render(
+        &self,
+        _api: &RenderApi,
+        _builder: &mut DisplayListBuilder,
+        _txn: &mut Transaction,
+        _framebuffer_size: DeviceUintSize,
+        _pipeline_id: PipelineId,
+        _document_id: DocumentId,
+    ) {
+        // XXX render frame queue
+    }
+
+    fn on_event(&self, _: winit::WindowEvent, _: &RenderApi, _: DocumentId) -> bool {
+        false
+    }
+
+    fn get_image_handlers(
+        &self,
+        _gl: &gl::Gl,
+    ) -> (
+        Option<Box<webrender::ExternalImageHandler>>,
+        Option<Box<webrender::OutputImageHandler>>,
+    ) {
+        (None, None)
+    }
+
+    fn draw_custom(&self, _gl: &gl::Gl) {}
+}
+
+impl FrameRenderer for App {
+    fn render(&self, frame: Frame) {
+        self.frame_queue.lock().unwrap().push(frame);
+    }
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let filename: &str = if args.len() == 2 {
+        args[1].as_ref()
+    } else {
+        panic!("Usage: cargo run --bin test-player <file_path>")
+    };
+
+    let path = Path::new(filename);
+    let player_wrapper = PlayerWrapper::new(&path);
+    let app = Arc::new(App::new());
+    player_wrapper.register_frame_renderer(app.clone());
+    ui::main_wrapper(app, None);
+    player_wrapper.shutdown();
+}
