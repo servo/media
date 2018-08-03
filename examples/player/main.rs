671196efe7b037747c172724ae93b16bc0bf379a
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-08-03 11:10:07 +0200

    Fix error message about test player usage

--- examples/player/main.rs
+++ examples/player/main.rs
@@ -228,7 +228,7 @@ fn main() {
     let filename: &str = if args.len() == 2 {
         args[1].as_ref()
     } else {
-        panic!("Usage: cargo run --bin test-player <file_path>")
+        panic!("Usage: cargo run --bin player <file_path>")
     };
 
     let path = Path::new(filename);
