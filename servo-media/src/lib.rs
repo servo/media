29e4280d4d5cb9ba1080aef76f1546947cfc5ba5
Manish Goregaokar <manishsmail@gmail.com>
2018-08-06 17:11:26 -0700

    x86_64, not x86

--- servo-media/src/lib.rs
+++ servo-media/src/lib.rs
@@ -1,5 +1,5 @@
 pub extern crate servo_media_audio as audio;
-#[cfg(all(not(target_os = "android"), target_arch = "x86"))]
+#[cfg(all(not(target_os = "android"), target_arch = "x86_64"))]
 extern crate servo_media_gstreamer;
 pub extern crate servo_media_player as player;
 use std::sync::{self, Arc, Mutex, Once};
@@ -37,9 +37,9 @@ impl PlayerBackend for DummyBackend {
     }
 }
 
-#[cfg(all(not(target_os = "android"), target_arch = "x86"))]
+#[cfg(all(not(target_os = "android"), target_arch = "x86_64"))]
 pub type Backend = servo_media_gstreamer::GStreamerBackend;
-#[cfg(not(all(not(target_os = "android"), target_arch = "x86")))]
+#[cfg(not(all(not(target_os = "android"), target_arch = "x86_64")))]
 pub type Backend = DummyBackend;
 
 impl ServoMedia {
