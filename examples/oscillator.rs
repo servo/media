3763c610dca252d61677768805d2749974dd522e
rkolhe <rkolhe@ncsu.edu>
2018-11-02 18:23:43 -0400

    added oscillator.rs

--- examples/oscillator.rs
+++ examples/oscillator.rs
@@ -2,11 +2,14 @@ extern crate servo_media;
 
 use servo_media::audio::node::{AudioNodeInit, AudioNodeMessage, AudioScheduledSourceNodeMessage};
 use servo_media::audio::oscillator_node::OscillatorNodeOptions;
+<<<<<<< HEAD
 use servo_media::audio::oscillator_node::OscillatorType::Sawtooth;
 use servo_media::audio::oscillator_node::OscillatorType::Triangle;
 //use servo_media::audio::oscillator_node::OscillatorType::Sine;
 use servo_media::audio::oscillator_node::OscillatorType::Custom;
 use servo_media::audio::oscillator_node::OscillatorType::Square;
+use servo_media::audio::oscillator_node::OscillatorType::Triangle;
+use servo_media::audio::param::{ParamType, RampKind, UserAutomationEvent};
 use servo_media::ServoMedia;
 use std::sync::Arc;
 use std::{thread, time};
@@ -22,7 +25,6 @@ fn run_example(servo_media: Arc<ServoMedia>) {
         osc1,
         AudioNodeMessage::AudioScheduledSourceNode(AudioScheduledSourceNodeMessage::Start(0.)),
     );
-
     thread::sleep(time::Duration::from_millis(3000));
     let _ = context.close();
     thread::sleep(time::Duration::from_millis(3000));
@@ -33,11 +35,12 @@ fn run_example(servo_media: Arc<ServoMedia>) {
     let osc2 = context.create_node(AudioNodeInit::OscillatorNode(options), Default::default());
     context.connect_ports(osc2.output(0), dest.input(0));
     let _ = context.resume();
+    thread::sleep(time::Duration::from_millis(3000));
+    context.connect_ports(osc2.output(0), dest.input(0));
     context.message_node(
         osc2,
         AudioNodeMessage::AudioScheduledSourceNode(AudioScheduledSourceNodeMessage::Start(0.)),
     );
-
     thread::sleep(time::Duration::from_millis(3000));
     let _ = context.close();
     thread::sleep(time::Duration::from_millis(1000));
