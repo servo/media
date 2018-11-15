d0df9beeaae4efef0b3b68702d87939c60007701
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2018-11-15 13:50:03 +0100

    gstreamer: map frames using VideoFrame API

--- backends/gstreamer/src/lib.rs
+++ backends/gstreamer/src/lib.rs
@@ -5,6 +5,7 @@ extern crate gstreamer as gst;
 extern crate gstreamer_app as gst_app;
 extern crate gstreamer_audio as gst_audio;
 extern crate gstreamer_player as gst_player;
+extern crate gstreamer_video as gst_video;
 extern crate ipc_channel;
 
 extern crate servo_media_audio;
