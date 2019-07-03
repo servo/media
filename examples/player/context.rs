2556fb714eac14228af78c22a199925b688c08ba
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2019-07-03 17:36:50 +0200

    Major rewrite of player sample
    Among other things, this rewrite includes:
    
    + Upgrade glutin and webrender
    + Mimics the current logic used in servo

---
+++ examples/player/context.rs
@@ -0,0 +1,107 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use servo_media::player::context::*;
+
+pub struct PlayerContextGlutin {
+    gl_context: GlContext,
+    native_display: NativeDisplay,
+    gl_api: GlApi,
+}
+
+impl PlayerContextGlutin {
+    pub fn new(
+        use_gl: bool,
+        windowed_context: &glutin::WindowedContext<glutin::PossiblyCurrent>,
+    ) -> Self {
+        if !use_gl {
+            return Self {
+                gl_context: GlContext::Unknown,
+                native_display: NativeDisplay::Unknown,
+                gl_api: GlApi::None,
+            };
+        }
+
+        let (gl_context, native_display, gl_api) = {
+            use glutin::os::ContextTraitExt;
+
+            let context = windowed_context.context();
+            let raw_handle = unsafe { context.raw_handle() };
+            let api = windowed_context.get_api();
+
+            #[cfg(any(
+                target_os = "linux",
+                target_os = "dragonfly",
+                target_os = "freebsd",
+                target_os = "netbsd",
+                target_os = "openbsd"
+            ))]
+            {
+                let gl_context = {
+                    use glutin::os::unix::RawHandle;
+
+                    match raw_handle {
+                        RawHandle::Egl(egl_context) => GlContext::Egl(egl_context as usize),
+                        RawHandle::Glx(glx_context) => GlContext::Glx(glx_context as usize),
+                    }
+                };
+                let native_display = if let Some(display) =
+                    unsafe { windowed_context.context().get_egl_display() }
+                {
+                    NativeDisplay::Egl(display as usize)
+                } else {
+                    use glutin::os::unix::WindowExt;
+
+                    if let Some(display) = windowed_context.window().get_wayland_display() {
+                        NativeDisplay::Wayland(display as usize)
+                    } else if let Some(display) = windowed_context.window().get_xlib_display() {
+                        NativeDisplay::X11(display as usize)
+                    } else {
+                        NativeDisplay::Unknown
+                    }
+                };
+
+                let gl_api = match api {
+                    glutin::Api::OpenGl => GlApi::OpenGL3,
+                    glutin::Api::OpenGlEs => GlApi::Gles2,
+                    _ => GlApi::None,
+                };
+
+                (gl_context, native_display, gl_api)
+            }
+
+            #[cfg(not(any(
+                target_os = "linux",
+                target_os = "dragonfly",
+                target_os = "freebsd",
+                target_os = "netbsd",
+                target_os = "openbsd"
+            )))]
+            {
+                println!("GL rendering unavailable for this platform");
+                (GlContext::Unknown, NativeDisplay::Unknown, GlApi::None)
+            }
+        };
+
+        Self {
+            gl_context,
+            native_display,
+            gl_api,
+        }
+    }
+}
+
+impl PlayerGLContext for PlayerContextGlutin {
+    fn get_gl_context(&self) -> GlContext {
+        self.gl_context.clone()
+    }
+
+    fn get_native_display(&self) -> NativeDisplay {
+        self.native_display.clone()
+    }
+
+    fn get_gl_api(&self) -> GlApi {
+        self.gl_api.clone()
+    }
+}
--- examples/player/main.rs
+++ examples/player/main.rs
@@ -4,275 +4,27 @@
 
 #[cfg(not(target_os = "android"))]
 extern crate clap;
+extern crate euclid;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
 extern crate gleam;
 #[cfg(not(target_os = "android"))]
 extern crate glutin;
 extern crate ipc_channel;
 extern crate servo_media;
-extern crate servo_media_auto;
-extern crate time;
 extern crate webrender;
+extern crate webrender_api;
 #[cfg(not(target_os = "android"))]
 extern crate winit;
 
-use gleam::gl;
-use servo_media::player::frame::{Frame, FrameRenderer};
 use servo_media::ServoMedia;
 use std::path::Path;
-use std::sync::{Arc, Mutex};
-#[cfg(not(target_os = "android"))]
-use ui::HandyDandyRectBuilder;
-use webrender::api::*;
 
 #[cfg(not(target_os = "android"))]
-#[path = "ui.rs"]
-mod ui;
-
-#[path = "player_wrapper.rs"]
-mod player_wrapper;
-
-struct FrameProvider {
-    frame_queue: Arc<Mutex<FrameQueue>>,
-    curr_frame: Option<Frame>,
-}
-
-impl webrender::ExternalImageHandler for FrameProvider {
-    fn lock(
-        &mut self,
-        _key: ExternalImageId,
-        _channel_index: u8,
-        _rendering: ImageRendering,
-    ) -> webrender::ExternalImage {
-        self.curr_frame = self.frame_queue.lock().unwrap().get();
-
-        let (id, height, width) = self
-            .curr_frame
-            .clone() // clone it because we want to keep the current texture alive
-            .and_then(|frame| {
-                Some((
-                    frame.get_texture_id(),
-                    frame.get_height(),
-                    frame.get_width(),
-                ))
-            })
-            .unwrap();
-
-        webrender::ExternalImage {
-            uv: TexelRect::new(0.0, 0.0, width as f32, height as f32),
-            source: webrender::ExternalImageSource::NativeTexture(id),
-        }
-    }
-    fn unlock(&mut self, _key: ExternalImageId, _channel_index: u8) {}
-}
-
-struct FrameQueue {
-    next_frame: Option<Frame>,
-    curr_frame: Option<Frame>,
-    prev_frame: Option<Frame>,
-    repaint: bool,
-}
-
-impl FrameQueue {
-    fn new() -> Self {
-        Self {
-            next_frame: None,
-            curr_frame: None,
-            prev_frame: None,
-            repaint: false,
-        }
-    }
-
-    fn add(&mut self, frame: Frame) {
-        let _ = self.next_frame.replace(frame);
-        self.repaint = true;
-    }
-
-    fn get(&mut self) -> Option<Frame> {
-        if self.is_empty() {
-            return None;
-        }
-
-        if self.next_frame.is_some() {
-            self.prev_frame = self.curr_frame.take();
-            self.curr_frame = self.next_frame.take();
-        } else {
-            if self.curr_frame.is_none() {
-                self.repaint = true;
-                return self.prev_frame.clone();
-            } else {
-                self.repaint = false;
-            }
-        }
-
-        self.curr_frame.clone()
-    }
-
-    fn prev(&mut self) -> Option<Frame> {
-        self.prev_frame.clone()
-    }
-
-    fn needs_repaint(&self) -> bool {
-        self.repaint
-    }
-
-    fn is_empty(&self) -> bool {
-        self.next_frame.is_none() && self.curr_frame.is_none() && self.next_frame.is_none()
-    }
-}
-
-struct App {
-    frame_queue: Arc<Mutex<FrameQueue>>,
-    image_key: Option<ImageKey>,
-    use_gl: bool,
-}
-
-impl App {
-    fn new() -> Self {
-        Self {
-            frame_queue: Arc::new(Mutex::new(FrameQueue::new())),
-            image_key: None,
-            use_gl: false,
-        }
-    }
-
-    fn init_image_key(&mut self, api: &RenderApi, txn: &mut Transaction, frame: &Frame) {
-        if self.image_key.is_some() {
-            return;
-        }
-
-        self.image_key = Some(api.generate_image_key());
-        let image_descriptor = ImageDescriptor::new(
-            frame.get_width(),
-            frame.get_height(),
-            ImageFormat::BGRA8,
-            false,
-            false,
-        );
-
-        if frame.is_gl_texture() {
-            txn.add_image(
-                self.image_key.clone().unwrap(),
-                image_descriptor,
-                ImageData::External(ExternalImageData {
-                    id: ExternalImageId(0),
-                    channel_index: 0,
-                    image_type: ExternalImageType::TextureHandle(TextureTarget::Default),
-                }),
-                None,
-            );
-        } else {
-            txn.add_image(
-                self.image_key.clone().unwrap(),
-                image_descriptor,
-                ImageData::new_shared(frame.get_data()),
-                None,
-            );
-        }
-    }
-}
-
-#[cfg(not(target_os = "android"))]
-impl ui::Example for App {
-    fn push_txn(
-        &mut self,
-        api: &RenderApi,
-        builder: &mut DisplayListBuilder,
-        txn: &mut Transaction,
-    ) {
-        if self.frame_queue.lock().unwrap().is_empty() {
-            return; /* we are not ready yet, sir */
-        }
-
-        let frame = self.frame_queue.lock().unwrap().get().unwrap();
-        let width = frame.get_width();
-        let height = frame.get_height();
-
-        if self.image_key.is_some() {
-            if let Some(old_frame) = self.frame_queue.lock().unwrap().prev() {
-                let old_width = old_frame.get_width();
-                let old_height = old_frame.get_height();
-                if (width != old_width) || (height != old_height) {
-                    txn.delete_image(self.image_key.unwrap());
-                    self.image_key = None;
-                }
-            }
-        }
-
-        if self.image_key.is_none() {
-            self.init_image_key(api, txn, &frame);
-        } else if !frame.is_gl_texture() {
-            txn.update_image(
-                self.image_key.clone().unwrap(),
-                ImageDescriptor::new(width, height, ImageFormat::BGRA8, false, false),
-                ImageData::new_shared(frame.get_data()),
-                &DirtyRect::All,
-            );
-        }
-
-        let bounds = (0, 0).to(width as i32, height as i32);
-        let info = LayoutPrimitiveInfo::new(bounds);
-        builder.push_stacking_context(
-            &info,
-            None,
-            TransformStyle::Flat,
-            MixBlendMode::Normal,
-            &[],
-            RasterSpace::Screen,
-        );
-
-        let image_size = LayoutSize::new(width as f32, height as f32);
-        let info = LayoutPrimitiveInfo::new(bounds);
-        builder.push_image(
-            &info,
-            image_size,
-            LayoutSize::zero(),
-            ImageRendering::Auto,
-            AlphaType::PremultipliedAlpha,
-            self.image_key.clone().unwrap(),
-            ColorF::WHITE,
-        );
-        builder.pop_stacking_context();
-    }
-
-    fn on_event(&self, _: winit::WindowEvent, _: &RenderApi, _: DocumentId) -> bool {
-        false
-    }
-
-    fn needs_repaint(&self) -> bool {
-        self.frame_queue.lock().unwrap().needs_repaint()
-    }
-
-    fn get_image_handlers(
-        &self,
-        _gl: &dyn gl::Gl,
-    ) -> (
-        Option<Box<dyn webrender::ExternalImageHandler>>,
-        Option<Box<dyn webrender::OutputImageHandler>>,
-    ) {
-        if !self.use_gl {
-            (None, None)
-        } else {
-            let queue = self.frame_queue.clone();
-            let provider = FrameProvider {
-                frame_queue: queue,
-                curr_frame: None,
-            };
-            (Some(Box::new(provider)), None)
-        }
-    }
-
-    fn draw_custom(&self, _gl: &dyn gl::Gl) {}
-
-    fn use_gl(&mut self, use_gl: bool) {
-        self.use_gl = use_gl;
-    }
-}
-
-impl FrameRenderer for App {
-    fn render(&mut self, frame: Frame) {
-        self.frame_queue.lock().unwrap().add(frame)
-    }
-}
+#[path = "app.rs"]
+mod app;
+use app::*;
 
 #[cfg(target_os = "android")]
 fn main() {
@@ -287,7 +39,7 @@ fn main() {
         .setting(clap::AppSettings::DisableVersion)
         .author("Servo developers")
         .about("Servo/MediaPlayer example using WebRender")
-        .usage("player [--gl|--no-video] <FILE>")
+        .usage("player [[--gl, --wr-stats]|--no-video] <FILE>")
         .arg(
             clap::Arg::with_name("gl")
                 .long("gl")
@@ -302,16 +54,29 @@ fn main() {
                 .help("Don't render video, only audio"),
         )
         .arg(
+            clap::Arg::with_name("wr-stats")
+                .long("wr-stats")
+                .display_order(3)
+                .help("Show WebRender profiler on screen")
+                .conflicts_with("no-video"),
+        )
+        .arg(
             clap::Arg::with_name("file")
                 .required(true)
                 .value_name("FILE"),
         )
         .get_matches();
 
-    let no_video = clap_matches.is_present("no-video");
-    let use_gl = clap_matches.is_present("gl");
+    let opts = Options {
+        no_video: clap_matches.is_present("no-video"),
+        use_gl: clap_matches.is_present("gl"),
+        wr_stats: clap_matches.is_present("wr-stats"),
+    };
+
     let path = clap_matches.value_of("file").map(|s| Path::new(s)).unwrap();
 
-    let app = Arc::new(Mutex::new(App::new()));
-    ui::main_wrapper(app, &path, no_video, use_gl, None);
+    match App::new(opts, path).and_then(main_loop).and_then(cleanup) {
+        Ok(r) => r,
+        Err(e) => eprintln!("Error! {}", e),
+    }
 }
