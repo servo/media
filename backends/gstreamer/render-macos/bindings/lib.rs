46c2468162f22326d708542f4099fbe5e264022a
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2019-09-24 18:44:50 +0200

    Refactored servo-media-gst-gl-macos-binding

--- backends/gstreamer/render-macos/bindings/lib.rs
+++ backends/gstreamer/render-macos/bindings/lib.rs
@@ -6,11 +6,40 @@
 extern crate glib;
 extern crate glib_sys;
 extern crate gstreamer as gst;
-extern crate gstreamer_gl as gst_gl;
-extern crate gstreamer_gl_sys as gst_gl_sys;
 extern crate gstreamer_sys as gst_sys;
+extern crate gstreamer_gl as gst_gl;
+extern crate servo_media_gst_gl_macos_bindings_sys as gst_gl_macos_sys;
 
-mod api;
-mod sys;
+use glib::translate::*;
 
-pub use api::*;
+macro_rules! assert_initialized_main_thread {
+    () => {
+        if unsafe { ::gst_sys::gst_is_initialized() } != ::glib_sys::GTRUE {
+            panic!("GStreamer has not been initialized. Call `gst::init` first.");
+        }
+    };
+}
+
+glib_wrapper! {
+    pub struct GLDisplayCocoa(Object<gst_gl_macos_sys::GstGLDisplayCocoa, gst_gl_macos_sys::GstGLDisplayCocoaClass, GLDisplayCocoaClass>) @extends gst_gl::GLDisplay, gst::Object;
+
+    match fn {
+        get_type => || gst_gl_macos_sys::gst_gl_display_cocoa_get_type(),
+    }
+}
+
+impl GLDisplayCocoa {
+    pub fn new() -> GLDisplayCocoa {
+        assert_initialized_main_thread!();
+        unsafe { from_glib_full(gst_gl_macos_sys::gst_gl_display_cocoa_new()) }
+    }
+}
+
+impl Default for GLDisplayCocoa {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+unsafe impl Send for GLDisplayCocoa {}
+unsafe impl Sync for GLDisplayCocoa {}
