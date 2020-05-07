76030c72a94bb85ccf5f6ae1be2a4c23d2ba61f2
Shamir Khodzha <khodzha.sh@gmail.com>
2020-05-07 12:49:00 +0300

    Eq on AudioNodeType

--- audio/node.rs
+++ audio/node.rs
@@ -37,7 +37,7 @@ pub enum AudioNodeInit {
 }
 
 /// Type of AudioNodeEngine.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq)]
 pub enum AudioNodeType {
     /// Not a constructable node
     AudioListenerNode,
