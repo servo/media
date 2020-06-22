c8dd51494be41300f61db3830a4a7f68b7020f59
Manish Goregaokar <manishsmail@gmail.com>
2020-06-22 15:02:32 -0700

    Add test for audio/video input stream

---
+++ examples/videoinput_stream.rs
@@ -0,0 +1,24 @@
+extern crate servo_media;
+extern crate servo_media_auto;
+
+use servo_media::ServoMedia;
+use std::sync::Arc;
+use std::{thread, time};
+
+fn run_example(servo_media: Arc<ServoMedia>) {
+    let stream = servo_media
+        .create_videoinput_stream(Default::default())
+        .unwrap();
+    let mut output = servo_media.create_stream_output();
+    output.add_stream(&stream);
+    thread::sleep(time::Duration::from_millis(6000));
+}
+
+fn main() {
+    ServoMedia::init::<servo_media_auto::Backend>();
+    if let Ok(servo_media) = ServoMedia::get() {
+        run_example(servo_media);
+    } else {
+        unreachable!();
+    }
+}
