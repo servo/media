8fd90fa29017733330307fb5b71591089fcc0825
Taym <haddadi.taym@gmail.com>
2024-08-13 11:32:20 +0200

    Update examples
    Signed-off-by: Taym <haddadi.taym@gmail.com>

--- examples/muted_audiocontext.rs
+++ examples/muted_audiocontext.rs
@@ -13,7 +13,7 @@ fn run_example(servo_media: Arc<ServoMedia>) {
     let context_id1 = &ClientContextId::build(1, 1);
     let context1 = servo_media.create_audio_context(&context_id1, Default::default());
     {
-        let context = context1.lock().unwrap();
+        let context = context1.unwrap().lock().unwrap();
         let dest = context.dest_node();
         let options = OscillatorNodeOptions::default();
         let osc1 = context.create_node(
@@ -33,7 +33,7 @@ fn run_example(servo_media: Arc<ServoMedia>) {
     {
         let mut options = OscillatorNodeOptions::default();
         options.oscillator_type = Sawtooth;
-        let context = context2.lock().unwrap();
+        let context = context2.unwrap().lock().unwrap();
         let dest = context.dest_node();
         let osc3 = context.create_node(
             AudioNodeInit::OscillatorNode(options.clone()),
